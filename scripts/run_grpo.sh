e012ee133ca5218ff98d38cb570920f02dcb70dd
tanghui01 <tanghui01@qianxin.com>
2025-02-21 19:51:10 +0800

    Add Unsloth optimization support for GRPO training

--- scripts/run_grpo.sh
+++ scripts/run_grpo.sh
@@ -4,5 +4,5 @@ export MKL_THREADING_LAYER=GNU
 export MKL_SERVICE_FORCE_INTEL=1
 
 ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/zero3.yaml \
-    --num_processes=4 src/open_r1/grpo.py \
+    --num_processes=6 src/open_r1/grpo.py \
     --config ./grpo_config.yaml
